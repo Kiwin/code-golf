79a30b1d77d5bdb36038aecaf2ad4cdd8d7e8bee
Jacker Deluxe <Kikoro@live.dk>
2019-11-05 11:00:56 +0100

    Add files via upload

---
+++ fizzbuzz/fizzbuzz_golf.js
@@ -0,0 +1,9 @@
+//fizzBuzz calculator
+fizzBuzz = i => {
+  let a = i % 3 == 0,
+    b = i % 5 == 0;
+  return (a ? "Fizz" : "") + (b ? "Buzz" : "") + (a || b ? "" : i);
+};
+
+//With loop (length: 112)
+for(let j=0;++j<100;)console.log((i=>{let a=i%3==0,b=i%5==0;return (a?"Fizz":"")+(b?"Buzz":"")+(a||b?"":i)})(j))
