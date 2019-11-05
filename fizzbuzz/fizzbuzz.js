2f673229a9a4fc51f35e526e0a891cfae6d5e23d
Jacker Deluxe <Kikoro@live.dk>
2019-11-05 11:02:26 +0100

    Update fizzbuzz.js

--- fizzbuzz/fizzbuzz.js
+++ fizzbuzz/fizzbuzz.js
@@ -5,5 +5,5 @@ fizzBuzz = i => {
   return (a ? "Fizz" : "") + (b ? "Buzz" : "") + (a || b ? "" : i);
 };
 
-//With loop (length: 112)
-for(let j=0;++j<100;)console.log((i=>{let a=i%3==0,b=i%5==0;return (a?"Fizz":"")+(b?"Buzz":"")+(a||b?"":i)})(j))
+//With loop (length: 111)
+for(let j=0;++j<100;)console.log((i=>{let a=i%3==0,b=i%5==0;return(a?"Fizz":"")+(b?"Buzz":"")+(a||b?"":i)})(j))
